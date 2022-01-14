7ba34ef7dd4462d5361cbce5fecc1470416c0729
shaymaebkn <chaymaeboukhana.solicode@gmail.com>
2022-01-14 13:02:40 +0100

    commit

--- api-ajax/script.js
+++ api-ajax/script.js
@@ -1,14 +1,15 @@
 
 var namee = document.querySelector("#name")
+var code = document.querySelector('#code')
 var population = document.querySelector(".population")
 var coderegion = document.querySelector(".code-region")
+
 
 document.querySelector("#cp").addEventListener('input', function(){
     if (this.value.length == 5  ) {
         let url = 
             `https://geo.api.gouv.fr/communes?codePostal=${this.value}&fields=nom,
              code,codesPostaux,codedepartement,codeRegion,population&format=json&geometry=centre`;
-            console.log(url)
             const xhr = new XMLHttpRequest();
             xhr.open("GET", url, true)
             xhr.onload = function (){
@@ -16,9 +17,13 @@ document.querySelector("#cp").addEventListener('input', function(){
 
                     let data = JSON.parse(this.responseText);
                     for (let ville of data){
-                        population.textContent = `${ville.population}`;
-                        coderegion.textContent = `${ville.codeRegion}`;
-                        namee.textContent = `${ville.nom}`;
+                        population.textContent = ville.population;
+                        coderegion.textContent = ville.codeRegion;
+                        namee.textContent = ville.nom;
+                        code.textContent = ville.code;
+                        
+
+    
                     }
 
                     }
